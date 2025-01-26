cd17a7d000138433ee47d4e2e846e9d0d373dd68
devscarycat <devscarycat@icloud.com>
2025-01-26 09:38:38 +0900

    [✨Feat] : 사용자 모델의 'followings' 필드에 blank=True 옵션 추가

--- users/models.py
+++ users/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class User(AbstractUser):
     followings = models.ManyToManyField(
-        "self", symmetrical=False, related_name="followers"
+        "self", symmetrical=False, related_name="followers", blank=True
     )
