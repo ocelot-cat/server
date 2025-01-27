94e86e36b8d74e6a4a2bf4733684f378e5eb59c8
devscarycat <devscarycat@icloud.com>
2025-01-27 16:10:42 +0900

    [✨Feat] : 활동 신호 추가 및 사용자 모델의 준비 메서드 구성

---
+++ activities/signals.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from posts.models import Post
+from users.models import User
+from .models import Activity
+
+
+@receiver(m2m_changed, sender=Post.likes.through)
+def create_like_activity(sender, instance, action, pk_set, **kwargs):
+    if action == "post_add":
+        for user_pk in pk_set:
+            Activity.objects.create(
+                actor=User.objects.get(pk=user_pk),
+                target_user=instance.author,
+                post=instance,
+                activity_type="like",
+            )
+
+
+@receiver(m2m_changed, sender=User.followings.through)
+def create_follow_activity(sender, instance, action, pk_set, **kwargs):
+    if action == "post_add":
+        for user_pk in pk_set:
+            Activity.objects.create(
+                actor=instance,
+                target_user=User.objects.get(pk=user_pk),
+                activity_type="follow",
+            )
