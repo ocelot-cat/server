25ad41d008d29e6376bfd953a6f202a91bb25987
devscarycat <devscarycat@icloud.com>
2025-03-09 23:11:19 +0900

    [✨Feat]: 부서 모델 및 관련 API 엔드포인트 추가, 회사 멤버십에 부서 필드 통합

--- companies/serializers.py
+++ companies/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, Department
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = ["id", "name", "owner"]
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = "__all__"
--- companies/urls.py
+++ companies/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import (
     CompanyMembersListView,
     CompanyPromoteMembersView,
+    DepartmentDetailView,
+    DepartmentView,
     InvitationCreateView,
     InvitationAcceptView,
     CompanyView,
@@ -21,6 +23,16 @@ urlpatterns = [
         "<int:company_id>/members/<int:user_id>/promote/",
         CompanyPromoteMembersView.as_view(),
         name="company_members_promote",
+    ),
+    path(
+        "<int:company_id>/departments/",
+        DepartmentView.as_view(),
+        name="create_department",
+    ),
+    path(
+        "departments/<int:pk>/",
+        DepartmentDetailView.as_view(),
+        name="department_detail",
     ),
     path(
         "<int:company_id>/invite/",
