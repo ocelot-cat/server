fe24c4b58c1bb7c7ada2ed14b687fa630f797064
devscarycat <devscarycat@icloud.com>
2025-04-14 03:41:58 +0900

    [🚇Railway]: Deploy

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
+        "NAME": config("PGDATABASE"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
