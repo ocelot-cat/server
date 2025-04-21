2884962a89101e519943e80cdb67b5088fe31c21
devscarycat <devscarycat@icloud.com>
2025-04-22 05:15:34 +0900

    [⚙️Setting] : Django 설정 파일 구조 개선 및 환경별 설정 추가

---
+++ config/settings/development.py
@@ -0,0 +1,18 @@
+from .base import BASE_DIR
+
+
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:19006",
+    "http://127.0.0.1:8000",
+    "https://imagedelivery.net",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
